fa3aca70f846c5b06dfc6302c1c27c0c3da5dc69
aoop-free <153523226+aoop-free@users.noreply.github.com>
2025-01-16 01:23:42 +0800

    Update main.sh

--- Proxy/main.sh
+++ Proxy/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Author: SuperManito
 ## Modified: 2025-01-04
-## 借鉴更新脚本 bash <(curl -sSL https://linuxmirrors.cn/main.sh)
+## 借鉴更新脚本: bash <(curl -sSL https://linuxmirrors.cn/main.sh)
 ## 更换软件源: 国内鸡专用
 
 ## 定制方法
